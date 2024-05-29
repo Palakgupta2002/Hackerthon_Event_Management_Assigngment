4fe5aacfd738259245daa3672cf14098edcc008d
Palak Gupta <120472704+Palakgupta2002@users.noreply.github.com>
2024-05-29 23:28:30 +0530

    Update api is done

--- Server/Routes/Hackerthon/Hackerthon.routes.js
+++ Server/Routes/Hackerthon/Hackerthon.routes.js
@@ -2,6 +2,7 @@ import express from "express"
 import User from "../../Modals/auth.js"
 const router = express.Router();
 import hackerthonSchema from "../../Modals/hackerthonSchema.js"
+import registrationSchema from "../../Modals/registrationSchema.js";
 
 const createHackerthon = async (req, res) => {
     const { name, email, company, date, description } = req.body;
@@ -74,6 +75,53 @@ const getAllHackerThons = async (req, res) => {
 }
 
 router.get('/getEmailHackerThon/:organizerEmail', getEmailHackerThon);
+
+const HackerThonEventDelete = async (req, res) => {
+    const { hackerthonId } = req.params;
+  
+    try {
+     
+      const hackerthonDelete = await hackerthonSchema.findByIdAndDelete(hackerthonId);
+  
+      if (!hackerthonDelete) {
+        return res.status(404).json({ message: "Hackathon not found" });
+      }
+  
+    
+      const registrationDelete = await registrationSchema.deleteMany({ hackerthonId });
+  
+     
+      res.json({ message: "Hackathon and associated registrations deleted successfully", hackerthonDelete, registrationDelete });
+    } catch (error) {
+      console.error("Error deleting hackathon event:", error);
+      res.status(500).json({ message: "An error occurred while deleting the hackathon event", error: error.message });
+    }
+  };
+  
+router.delete('/deleteHackerthon/:hackerthonId',HackerThonEventDelete)
+
+router.put('/updateHackerthon/:id', async (req, res) => {
+    const { id } = req.params;
+    const { name, company, date, description } = req.body;
+
+    try {
+        const updatedHackerthon = await hackerthonSchema.findByIdAndUpdate(
+            id,
+            { name, company, date, description },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedHackerthon) {
+            return res.status(404).json({ message: 'Hackathon not found' });
+        }
+
+        res.json({ message: 'Hackathon updated successfully', hackerthon: updatedHackerthon });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred while updating the hackathon', error: error.message });
+    }
+});
+
 
 
 export default router
